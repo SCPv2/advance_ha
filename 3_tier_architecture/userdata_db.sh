1c1d305dd7fed0d1aa18c486e586fe0a72eb954d
SCPuser <revotty@gmail.com>
2025-08-16 20:13:33 +0900

    update

--- 3_tier_architecture/userdata_db.sh
+++ 3_tier_architecture/userdata_db.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
-sudo dnf -y update
-sudo dnf -y upgrade
 sudo dnf install -y epel-release
+sudo dnf -y update
 sudo dnf install -y wget curl git vim nano htop net-tools bind-utils netcat
+sudo -u rocky git clone https://github.com/SCPv2/ceweb.git
+cd /home/rocky/ceweb/db-server/vm_db
+sudo bash install_postgresql_vm.sh
